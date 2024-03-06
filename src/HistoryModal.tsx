25bd15e2aca07eeb2c0500afdb035cfcd1515542
Tyler Goodwin <tyler.goodwin23@gmail.com>
2024-03-06 21:50:55 +1100

    Add leaderboard in history

--- src/HistoryModal.tsx
+++ src/HistoryModal.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment, useEffect, useRef } from "react";
+import { FC, Fragment, useEffect, useMemo, useRef } from "react";
 import { createPortal } from "react-dom";
 import { useGameHistory } from "./useGameHistory";
 import "./HistoryModal.css";
@@ -22,6 +22,28 @@ export const HistoryModal: FC<Props> = (props) => {
     }
   }, [open]);
 
+  const summarized = useMemo(() => {
+    const collated = props.history.games.reduce<Record<string, number>>(
+      (acc, game) => {
+        game.scores.forEach((g) => {
+          if (acc[g.name] === undefined) {
+            acc[g.name] = 0;
+          }
+          acc[g.name] += g.score;
+        });
+        return acc;
+      },
+      {}
+    );
+
+    return Object.entries(collated)
+      .map<{ name: string; score: number }>(([name, score]) => ({
+        name,
+        score,
+      }))
+      .sort((a, b) => b.score - a.score);
+  }, [props.history.games]);
+
   return createPortal(
     <dialog className="Modal" ref={dialogRef}>
       <div className="ModalHeader">
@@ -37,18 +59,32 @@ export const HistoryModal: FC<Props> = (props) => {
           Close
         </button>
       </div>
-      <div className="PastGamesList">
-        <span>Date</span>
-        <span>Winner</span>
-        <span>Score</span>
-        <div className="GameEntry">
-          {props.history.games.map((g, i) => (
+      <div className="Leaderboard">
+        <h3>All-Time Leaderboard</h3>
+        <div className="Leaderboard__list">
+          {summarized.map((s, i) => (
             <Fragment key={i}>
-              <span>{g.date}</span>
-              <span>{g.scores[0]?.name}</span>
-              <span>{g.scores[0]?.score}</span>
+              <span>{s.name}</span>
+              <span className="right-align">{s.score}</span>
             </Fragment>
           ))}
+        </div>
+      </div>
+      <div>
+        <h3>Past Games</h3>
+        <div className="PastGamesList">
+          <span>Date</span>
+          <span>Winner</span>
+          <span>Score</span>
+          <div className="GameEntry">
+            {props.history.games.map((g, i) => (
+              <Fragment key={i}>
+                <span>{g.date}</span>
+                <span>{g.scores[0]?.name}</span>
+                <span className="right-align">{g.scores[0]?.score}</span>
+              </Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </dialog>,
